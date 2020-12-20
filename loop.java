6386e545d3af78ea08b0d81902c96c927ef28f7d
Shivam Bhatia <shivambhatia270@gmail.com>
2020-12-20 13:50:35 +0530

    This is commit for java file

---
+++ loop.java
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+// Java program to illustrate for loop 
+class forLoopDemo { 
+    public static void main(String args[]) 
+    { 
+        // Writing a for loop 
+        // to print Hello World 5 times 
+        for (int i = 1; i <= 5; i++) 
+            System.out.println("Heelo World"); 
+    } 
+} 
+=======
+
+>>>>>>> 34f2760b2fbb9dc979b95337aa563e2326038071
